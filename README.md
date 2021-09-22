27ca926f591c74af5b8fd62d7514bc7cc23ef4e0
Martin Canario <m.canario01@ufromail.cl>
2021-09-21 23:25:07 -0300

    update

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Segundo semestre del año 2021.
 
 En este repositorio guardaré archivos de el ramo Estructura de datos y algoritmos de la UFRO.
-cambio
 
 [Abrir Notion de EDA](https://madac.notion.site/f28bfdf9c0b84cafba3b54b0a34f640a?v=93e4b3a6bf394c2ca06774c1b2e9db20)
 
--- clase2/ejercicios/mayor_de_2_numeros/main.cpp
+++ clase2/ejercicios/mayor_de_2_numeros/main.cpp
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     printf("\nEste programa leerá 2 numeros y reponderá el mayor de los 2.\n");
     printf("Por favor, ingrese un numero entero: ");
-    int a,b;
-    scanf("%d",&a);
+    int a, b;
+    scanf("%d", &a);
     printf("\nIngrese un segundo numero entero: ");
-    scanf("%d",&b);
+    scanf("%d", &b);
     if (a > b)
     {
-        printf("\n%d es el numero más grande.", a); 
-    }else if (b > a)
+        printf("\n%d es el numero más grande.", a);
+    }
+    else if (b > a)
     {
-        printf("\n%d es el numero más grande.", b); 
-    }else{
+        printf("\n%d es el numero más grande.", b);
+    }
+    else
+    {
         printf("\nAmbos numeros son del mismo valor.");
     }
-    
-    
+
     return 0;
 }
